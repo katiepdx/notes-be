68704c702d7d81922cb7b51cadc672be33ecec8e
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-11-04 17:49:55 -0800

    add post route and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,20 @@ describe('notes-be routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
   });
+
+  it('should add a note to the database table using POST', async () => {
+    return await request(app)
+      .post('/api/v1/notes')
+      .send({
+        topic: 'JavaScript',
+        note: 'My JavaScript note'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          topic: 'JavaScript',
+          note: 'My JavaScript note'
+        })
+      })
+  })
 });
